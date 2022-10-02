d026ad3c22319dd75aaac8904967b1c42816426a
SAyaka Bhandari <sayaka.bhandari@kcl.ac.uk>
2022-10-02 22:44:18 +0100

    second try to match the test cases

--- median.py
+++ median.py
@@ -6,9 +6,9 @@ while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
-        print (statistics.median(numbers))
+        
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+print (statistics.median(numbers))
